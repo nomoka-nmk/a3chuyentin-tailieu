892394d5b4ab703d30de391cb9a6bdfb0b11d00b
nomoka.nmk <nomoka.nmk@proton.me>
2025-06-20 01:32:31 +0700

    Update một xíu

--- document_manager.py
+++ document_manager.py
@@ -292,7 +292,7 @@ class DocumentManager(ctk.CTk):
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>A3 Chuyên Tin - Tài Liệu {doc['id']}: {doc['displayName']}</title>
+    <title>{doc['displayName']}</title>
     <meta name="description" content="{doc['description']}">
     <meta name="keywords" content="{', '.join(doc['tags'])}, A3 Chuyên Tin, tài liệu học tập">
     <meta name="author" content="A3 Chuyên Tin">
